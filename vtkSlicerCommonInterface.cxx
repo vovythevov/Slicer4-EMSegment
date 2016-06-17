vovythevov/Slicer4-EMSegment

commit 8d42c304aef55d6c36842054806cb23b7d386822

ENH: Simplify import of Logic/MRML/DisplayableManager python modules
This commit updates vtkSlicerCommonInterface::Startup method to comply with
changes introduced in Slicer r25128

git-svn-id: http://svn.slicer.org/Slicer3/branches/Slicer4-EMSegment@17117 3bd1e089-480b-0410-8dfb-8563597acbee

vtkSlicerCommonInterface.cxx
@@ -118,13 +118,12 @@ vtkSlicerCommonInterface::~vtkSlicerCommonInterface()
     // Update current application directory, so that *PythonD modules can be loaded
     ctkScopedCurrentDir scopedCurrentDir(emsegmentModulePath);
 
-    QString emsegmentModulePythonPath = emsegmentModulePath + "/Python";
-    std::cout << "emsegmentModulePythonPath:" << qPrintable(emsegmentModulePythonPath) << std::endl;
+    std::cout << "emsegmentModulePath:" << qPrintable(emsegmentModulePath) << std::endl;
     app->pythonManager()->executeString(QString(
       "from slicer.util import importVTKClassesFromDirectory;"
-      "importVTKClassesFromDirectory('%1', 'slicer', filematch='vtkSlicer%2ModuleLogic.py');"
-      "importVTKClassesFromDirectory('%1', 'slicer', filematch='vtkSlicer%2ModuleMRML.py');"
-      ).arg(emsegmentModulePythonPath).arg("EMSegment"));
+      "importVTKClassesFromDirectory('%1', 'slicer', filematch='vtkSlicer%2ModuleLogicPython.*');"
+      "importVTKClassesFromDirectory('%1', 'slicer', filematch='vtkSlicer%2ModuleMRMLPython.*');"
+      ).arg(emsegmentModulePath).arg("EMSegment"));
 
     }
 #endif

commit 353bed96de3f0ea9173bfafabd840a576b4cfeac

BUG: Fix import of python modules.
This commit fixes a regression introduced in r17117 (ENH: Simplify import
of Logic/MRML/DisplayableManager python modules). It ensures python modules
built on system using intermediate directories (e.g Visual Studio) can 
be imported.

git-svn-id: http://svn.slicer.org/Slicer3/branches/Slicer4-EMSegment@17121 3bd1e089-480b-0410-8dfb-8563597acbee

vtkSlicerCommonInterface.cxx
@@ -114,6 +114,10 @@ vtkSlicerCommonInterface::~vtkSlicerCommonInterface()
     QString emsegmentModulePath = QString::fromStdString(
         vtkSlicerApplicationLogic::GetModuleSlicerXYLibDirectory(argv[0]));
     emsegmentModulePath.append("/" Slicer_QTLOADABLEMODULES_SUBDIR);
+    if(!app->intDir().isEmpty())
+      {
+      emsegmentModulePath.append("/" + app->intDir());
+      }
     app->pythonManager()->appendPythonPath(emsegmentModulePath);
     // Update current application directory, so that *PythonD modules can be loaded
     ctkScopedCurrentDir scopedCurrentDir(emsegmentModulePath);